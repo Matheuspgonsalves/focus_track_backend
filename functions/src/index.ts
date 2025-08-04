21a303a0e78780785c8a6860188a4fbf2a975c99
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-08-03 21:07:34 -0300

    setup: mais configurações de projeto

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as express from "express";
+import * as functions from "firebase-functions";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = express();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+app.get("/");
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.app = functions.runWith({timeoutSeconds: 540, memory: "8GB"}).https.onRequest(app);
